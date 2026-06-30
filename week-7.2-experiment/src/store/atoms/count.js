e1f7a16a9f4b2f3d5911272eda13fafd4612f5cd
dj-karthick <karthickallancherry@gmail.com>
2026-06-30 12:48:11 +0530

    Learn about the selectors depend on more than one state or atom from todo assignment example

--- week-7.2-experiment/src/store/atoms/count.js
+++ week-7.2-experiment/src/store/atoms/count.js
@@ -13,3 +13,13 @@ export const evenSelector = selector({  //Defining the selector
         return count % 2;
     }
 })
+
+export const filterTodos = selector({   //Selector using two dependencis.
+    key: "filterTodos",
+    get: ({get}) => {
+        const todos = get(todosAtom);
+        const filter = get(filterAtom);
+
+        return todos.filter(x => x.title.includes(filter) || x.description.includes(filter))
+    }
+})
