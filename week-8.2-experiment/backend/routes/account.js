2adab0ff545a1bd4de99f8825f64b70c704ed56a
dj-karthick <karthickallancherry@gmail.com>
2026-07-05 10:11:50 +0530

    Wrap the Transfer route in account route inside the Transaction

--- week-8.2-experiment/backend/routes/account.js
+++ week-8.2-experiment/backend/routes/account.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
+const Account = require('../db');
+const mongoose = require('mongoose');
 
 router.get("/balance", authMiddleware, async (req, res) => {
     const account = await Account.findOne({
@@ -13,27 +15,43 @@ router.get("/balance", authMiddleware, async (req, res) => {
 })
 
 router.post('/transfer', authMiddleware, async () => {          // Making transaction without using MondoDb Transaction.
+
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
     const { amount, to } = req.body;
     
+    //Fetch the accounts within the transaction.
     const account = await Account.findOne({
         userId: req.userId
-    })
+    }).session(session);
 
-    if(account.balance < amount) return res.status(400).json({msg: "Insufficient balance"});
+    if(!account || account.balance < amount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            msg: "Insufficient balance"
+        })
+    }
 
     const toAccount = await Account.findOne({
         userId: to
-    })
+    }).session(session);
 
-    if(!toAccount) return res.status(400).json({msg: "Invalid Account"});
+    if(!toAccount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            msg: "Invalid Account"
+        })
+    }
 
+    //Perform the transfer.
     await Account.updateOne({
         userId: req.userId
     }, {
         $inc: {
             balance: -amount
         }
-    })
+    }).session(session);
 
     await Account.updateOne({
         userId: to
@@ -41,7 +59,10 @@ router.post('/transfer', authMiddleware, async () => {          // Making transa
         $inc: {
             balance: amount
         }
-    })
+    }).session(session);
+
+    //Commit the transaction.
+    await session.commitTransaction();
 
     res.json({
         msg: "Transfer successful"
