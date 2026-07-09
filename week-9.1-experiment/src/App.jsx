1dbf1684ba2e3218011435be73ac61c41701d822
dj-karthick <karthickallancherry@gmail.com>
2026-07-09 17:48:35 +0530

    Created the useDebounce hook and experimented how this works

--- week-9.1-experiment/src/App.jsx
+++ week-9.1-experiment/src/App.jsx
@@ -2,27 +2,34 @@ import { use, useEffect, useState } from 'react'
 import React from 'react'
 import './App.css'
 
-function useInterval(fn, timeInput){
+function useDebounce(value, timeOut){
+  const [debouncedValue, setDebouncedValue] = useState(value);
   
   useEffect(()=>{
-    setInterval(() => {
-      fn()
-    }, timeInput);
-  }, [])
+    const timeOutNumber = setTimeout(() => {
+      setDebouncedValue(value)
+    }, timeOut);
+    return ()=>{
+      clearTimeout(timeOutNumber)
+    }
+  }, [value])
 
+  return debouncedValue;
 }
 
 function App() {
-  
-  const [count, setCount] = useState(0);
 
-  useInterval(()=>{
-    setCount(c => c + 1)
-  }, 1000)
-  
+  const [value, setValue] = useState('');
+  const debouncedValue = useDebounce(value, 5000);  
+  console.log(debouncedValue);
+
+  // useEffect(()=>{
+  //   axios.get() 
+  // }, [debouncedValue])
 
   return <>
-    Timer is at {count}
+    Debounced value is {debouncedValue}
+    <input type="text" value={value} onChange={e => setValue(e.target.value)} />
   </>
 
 
