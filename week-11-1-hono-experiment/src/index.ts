67016d7b419d0b3ec60d872137da5ea9a44718d7
dj-karthick <karthickallancherry@gmail.com>
2026-08-04 14:42:50 +0530

    Topics are:
    Serverless Functions -> Why this -> Architecture -> Cloud Providers -> Deployed to CloudFlare
    -> Hono -> Get the all the things needed like express app -> Slight Db

---
+++ week-11-1-hono-experiment/src/index.ts
@@ -0,0 +1,19 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post('/', async (c) => {
+  let body: any;
+
+  try{
+    body = await c.req.json();
+  }catch{}
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  
+  return c.text('Hello Hono!')
+})
+
+export default app
